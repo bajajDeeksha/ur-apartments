30c71d472851652044a8156babeec573c0b20f8f
fssamudra <samudra@fullspeed.co.jp>
2017-03-06 16:00:47 +0900

    adding php code, user management

---
+++ config/Migrations/20170306060517_Users.php
@@ -0,0 +1,67 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('users');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('username', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('auth', 'integer', [
+            'default' => null,
+            'limit' => 4,
+            'null' => true,
+        ]);
+        $table->addColumn('validity', 'integer', [
+            'default' => 1,
+            'limit' => 11,
+            'null' => true,
+        ]);
+        $table->addColumn('state', 'integer', [
+            'default' => null,
+            'limit' => 4,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->create();
+    }
+
+    public function down()
+    {
+        $table = $this->table('users');
+        $table->drop();
+    }
+}
